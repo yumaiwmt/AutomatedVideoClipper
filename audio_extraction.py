4bf43c6b4978bba37e0f5663717a4c62ae5ae75d
Yuma710 <yuma.iwamoto@mail.utoronto.ca>
2026-03-08 00:45:32 -0500

    complete audio extraction

--- audio_extraction.py
+++ audio_extraction.py
@@ -1,15 +1,17 @@
-import whisper
-from moviepy.editor import VideoFileClip
+from moviepy import VideoFileClip
 import os
 
 def audio_extraction(video_path, output_audio_path="audio.mp3"):
     video = VideoFileClip(video_path)
-    audio = video.audio
+    audio = video.audio.write_audiofile(output_audio_path, fps=16000, logger=None)
     
-    audio.write_audiofile(output_audio_path, logger=None)
-    
-    audio.close()
     video.close()
     return output_audio_path
 
-audio_file = audio_extraction("input_video.mp4")
+if __name__ == "__main__":
+    input_video = "short_test_vid.mp4"
+    if os.path.exists(input_video):
+        audio_file = audio_extraction(input_video)
+        print(f"抽出完了: {audio_file}")
+    else:
+        print(f"エラー: {input_video} が見つかりません。")
